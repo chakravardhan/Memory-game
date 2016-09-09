5844078f0d4550a4911244e86f1c98a13955c4b9
Chakravardhan Reddy <Chakravardhan Reddy>
2016-09-09 23:58:34 +0530

    fixed errors

--- app/src/main/java/com/example/chakravardhan/mygame/engine/Engine.java
+++ app/src/main/java/com/example/chakravardhan/mygame/engine/Engine.java
@@ -14,6 +14,11 @@ import com.example.chakravardhan.mygame.common.Shared;
 import com.example.chakravardhan.mygame.engine.ScreenController.Screen;
 import com.example.chakravardhan.mygame.events.EventObserverAdapter;
 
+import com.example.chakravardhan.mygame.events.ui.BackGameEvent;
+import com.example.chakravardhan.mygame.events.ui.DifficultySelectedEvent;
+import com.example.chakravardhan.mygame.events.ui.FlipCardEvent;
+import com.example.chakravardhan.mygame.events.ui.NextGameEvent;
+import com.example.chakravardhan.mygame.events.ui.ResetBackgroundEvent;
 import com.example.chakravardhan.mygame.events.ui.StartEvent;
 import com.example.chakravardhan.mygame.events.ui.ThemeSelectedEvent;
 
@@ -43,7 +48,13 @@ public class Engine extends EventObserverAdapter {
 	}
 
 	public void start() {
+		Shared.eventBus.listen(DifficultySelectedEvent.TYPE, this);
+		Shared.eventBus.listen(FlipCardEvent.TYPE, this);
 		Shared.eventBus.listen(StartEvent.TYPE, this);
+		Shared.eventBus.listen(ThemeSelectedEvent.TYPE, this);
+		Shared.eventBus.listen(BackGameEvent.TYPE, this);
+		Shared.eventBus.listen(NextGameEvent.TYPE, this);
+		Shared.eventBus.listen(ResetBackgroundEvent.TYPE, this);;
 	}
 
 	public void stop() {
@@ -52,15 +63,47 @@ public class Engine extends EventObserverAdapter {
 		mHandler.removeCallbacksAndMessages(null);
 		mHandler = null;
 
+		Shared.eventBus.unlisten(DifficultySelectedEvent.TYPE, this);
+		Shared.eventBus.unlisten(FlipCardEvent.TYPE, this);
 		Shared.eventBus.unlisten(StartEvent.TYPE, this);
+		Shared.eventBus.unlisten(ThemeSelectedEvent.TYPE, this);
+		Shared.eventBus.unlisten(BackGameEvent.TYPE, this);
+		Shared.eventBus.unlisten(NextGameEvent.TYPE, this);
+		Shared.eventBus.unlisten(ResetBackgroundEvent.TYPE, this);
 
 		mInstance = null;
 	}
 
+	@Override
+	public void onEvent(ResetBackgroundEvent event) {
+		Drawable drawable = mBackgroundImage.getDrawable();
+		if (drawable != null) {
+			((TransitionDrawable) drawable).reverseTransition(2000);
+		} else {
+			new AsyncTask<Void, Void, Bitmap>() {
+
+				@Override
+				protected Bitmap doInBackground(Void... params) {
+					Bitmap bitmap = Utils.scaleDown(R.drawable.background, Utils.screenWidth(), Utils.screenHeight());
+					return bitmap;
+				}
+
+				protected void onPostExecute(Bitmap bitmap) {
+					mBackgroundImage.setImageBitmap(bitmap);
+				};
+
+			}.execute();
+		}
+	}
 
 	@Override
 	public void onEvent(StartEvent event) {
 		mScreenController.openScreen(Screen.THEME_SELECT);
+	}
+	@Override
+	public void onEvent(BackGameEvent event) {
+//		PopupManager.closePopup();
+		mScreenController.openScreen(Screen.DIFFICULTY);
 	}
 	@Override
 	public void onEvent(ThemeSelectedEvent event) {
--- app/src/main/java/com/example/chakravardhan/mygame/events/EventObserverAdapter.java
+++ app/src/main/java/com/example/chakravardhan/mygame/events/EventObserverAdapter.java
@@ -1,20 +1,65 @@
 package com.example.chakravardhan.mygame.events;
 
+import com.example.chakravardhan.mygame.events.engine.FlipDownCardsEvent;
+import com.example.chakravardhan.mygame.events.engine.GameWonEvent;
+import com.example.chakravardhan.mygame.events.engine.HidePairCardsEvent;
+import com.example.chakravardhan.mygame.events.ui.BackGameEvent;
+import com.example.chakravardhan.mygame.events.ui.DifficultySelectedEvent;
+import com.example.chakravardhan.mygame.events.ui.FlipCardEvent;
+import com.example.chakravardhan.mygame.events.ui.NextGameEvent;
+import com.example.chakravardhan.mygame.events.ui.ResetBackgroundEvent;
 import com.example.chakravardhan.mygame.events.ui.StartEvent;
 import com.example.chakravardhan.mygame.events.ui.ThemeSelectedEvent;
 
 
 public class EventObserverAdapter implements EventObserver {
 
+	public void onEvent(FlipCardEvent event) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void onEvent(DifficultySelectedEvent event) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void onEvent(HidePairCardsEvent event) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void onEvent(FlipDownCardsEvent event) {
+		throw new UnsupportedOperationException();
+	}
 
 	@Override
 	public void onEvent(StartEvent event) {
 		throw new UnsupportedOperationException();
 	}
 
-
 	@Override
 	public void onEvent(ThemeSelectedEvent event) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void onEvent(GameWonEvent event) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void onEvent(BackGameEvent event) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void onEvent(NextGameEvent event) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void onEvent(ResetBackgroundEvent event) {
 		throw new UnsupportedOperationException();
 	}
 }
