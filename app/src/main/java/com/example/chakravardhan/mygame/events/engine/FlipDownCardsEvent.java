068d710303b4916a51da0bf5a4c1f1eadfbf6125
Praneeth <spraneethnagu@gmail.com>
2016-09-09 22:26:46 +0530

    added events

---
+++ app/src/main/java/com/example/chakravardhan/mygame/events/engine/FlipDownCardsEvent.java
@@ -0,0 +1,26 @@
+package com.example.chakravardhan.mygame.events.engine;
+
+import com.example.chakravardhan.mygame.events.AbstractEvent;
+import com.example.chakravardhan.mygame.events.EventObserver;
+
+/**
+ * When the 'back to menu' was pressed.
+ */
+public class FlipDownCardsEvent extends AbstractEvent {
+
+	public static final String TYPE = FlipDownCardsEvent.class.getName();
+
+	public FlipDownCardsEvent() {
+	}
+	
+	@Override
+	protected void fire(EventObserver eventObserver) {
+		eventObserver.onEvent(this);
+	}
+
+	@Override
+	public String getType() {
+		return TYPE;
+	}
+
+}
